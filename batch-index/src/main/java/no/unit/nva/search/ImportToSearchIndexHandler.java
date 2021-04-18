11c1bc979b6ee5ccc48cd1b0b92daf1072917233
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-04-18 22:37:06 +0200

    add logging file

--- batch-index/src/main/java/no/unit/nva/search/ImportToSearchIndexHandler.java
+++ batch-index/src/main/java/no/unit/nva/search/ImportToSearchIndexHandler.java
@@ -69,9 +69,10 @@ public class ImportToSearchIndexHandler implements RequestStreamHandler {
         String inputString = IoUtils.streamToString(input);
         ImportDataRequest request = JsonUtils.objectMapper.readValue(inputString, ImportDataRequest.class);
         setupS3Access(request.getBucket());
-        Stream<Publication> publishedPublications = fetchPublishedPublicationsFromDynamoDbExportInS3(request);
+        List<Publication> publishedPublications = fetchPublishedPublicationsFromDynamoDbExportInS3(request)
+            .collect(Collectors.toList());
 
-        List<Try<SortableIdentifier>> indexActions = insertToIndex(publishedPublications)
+        List<Try<SortableIdentifier>> indexActions = insertToIndex(publishedPublications.stream())
                                                          .collect(Collectors.toList());
 
         List<String> failures = collectFailures(indexActions.stream());
