af42abaeea6f75b0a12df7850b14ac93d63c107e
Orestis Gkorgkas <orestis.gkorgkas@unit.no>
2021-10-20 12:38:30 +0200

    verbose inputstream check

--- search-commons/src/main/java/no/unit/nva/search/utils/SearchIndexFrame.java
+++ search-commons/src/main/java/no/unit/nva/search/utils/SearchIndexFrame.java
@@ -1,12 +1,12 @@
 package no.unit.nva.search.utils;
 
-import java.nio.file.Path;
-import nva.commons.core.ioutils.IoUtils;
+import static nva.commons.core.ioutils.IoUtils.inputStreamFromResources;
+import static nva.commons.core.ioutils.IoUtils.streamToString;
 
 public final class SearchIndexFrame {
 
     public static final String FRAME_JSON = "publication_frame.json";
-    public static final String FRAME_SRC = IoUtils.stringFromResources(Path.of(FRAME_JSON));
+    public static final String FRAME_SRC = streamToString(inputStreamFromResources(FRAME_JSON));
 
     private SearchIndexFrame() {
 
