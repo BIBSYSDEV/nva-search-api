c4c0c46cbb5608f9bb299bf84e68c8f6fe736d76
Eyob Teweldemedhin <eyob.teweldemedhin@sikt.no>
2023-06-26 13:57:23 +0200

    Set results size in environment variabel.

--- search-resources-api/src/main/java/no/unit/nva/search/RequestUtil.java
+++ search-resources-api/src/main/java/no/unit/nva/search/RequestUtil.java
@@ -3,6 +3,7 @@ package no.unit.nva.search;
 import no.unit.nva.search.models.SearchDocumentsQuery;
 import no.unit.nva.search.models.SearchTicketsQuery;
 import nva.commons.apigateway.RequestInfo;
+import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 import nva.commons.core.paths.UriWrapper;
 import org.opensearch.search.aggregations.AbstractAggregationBuilder;
@@ -29,6 +30,7 @@ public class RequestUtil {
     public static final String PATH = "path";
     public static final String DOMAIN_NAME = "domainName";
     public static final String HTTPS = "https";
+    public static final Environment ENVIRONMENT = new Environment();
 
     /**
      * Get searchTerm from request query parameters.
@@ -45,7 +47,7 @@ public class RequestUtil {
     }
 
     public static int getExportResults(RequestInfo requestInfo) {
-        return Integer.parseInt(requestInfo.getQueryParameters().getOrDefault(RESULTS_KEY, "100"));
+        return Integer.parseInt(requestInfo.getQueryParameters().getOrDefault(RESULTS_KEY, ENVIRONMENT.readEnv("EXPORT_SEARCH_RESULTS_SIZE")));
     }
 
     public static int getFrom(RequestInfo requestInfo) {
