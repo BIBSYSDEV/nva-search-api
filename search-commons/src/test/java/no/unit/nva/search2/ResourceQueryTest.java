f5f4846b75f2e5f34c13ab0d7060554889b0a8b1
Stig Norland <stig.norland@sikt.no>
2023-08-30 15:55:14 +0200

    https://unit.atlassian.net/browse/NP-45163 transfere to work

--- search-commons/src/test/java/no/unit/nva/search2/ResourceQueryTest.java
+++ search-commons/src/test/java/no/unit/nva/search2/ResourceQueryTest.java
@@ -4,8 +4,14 @@ import nva.commons.apigateway.exceptions.BadRequestException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.net.URI;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,14 +26,24 @@ class ResourceQueryTest {
     void tearDown() {
     }
 
-    @Test
-    void builder(Map<String, String> parameters) throws BadRequestException {
+    @ParameterizedTest
+    @MethodSource("uriProvider")
+    void builder(URI uri) throws BadRequestException {
+        var params = Arrays.stream(uri.getQuery().split("&")).map(s -> s.split("=")).collect(
+            Collectors.toMap(s -> s[0], s -> s[1]));
         var test = ResourceQuery.builder()
-            .fromQueryParameters(parameters)
+            .fromQueryParameters(params)
             .build();
+        var uri2 = test.toURI();
+        System.out.println(test.toURI().toString());
+        assertEquals(uri,uri2);
 
-        var uri = test.toURI()
+    }
 
-
+    static Stream<URI> uriProvider() {
+        return Stream.of(
+            URI.create("https://example.com/?category=hello+world&lang=en"),
+            URI.create("https://example.com/?category=hello+world&lang=en"),
+            URI.create("https://example.com/?category=hello+world&lang=en"));
     }
 }
--- search-commons/src/test/java/no/unit/nva/search2/SwsOpenSearchClientTest.java
+++ search-commons/src/test/java/no/unit/nva/search2/SwsOpenSearchClientTest.java
@@ -1,10 +1,11 @@
 package no.unit.nva.search2;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.net.URI;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,15 +13,22 @@ class SwsOpenSearchClientTest {
 
     private SwsOpenSearchClient swsOpenSearchClient;
 
-    @BeforeAll
+    @BeforeEach
     void setUp() {
         swsOpenSearchClient = SwsOpenSearchClient.defaultSwsClient();
     }
 
-    @Test
     @ParameterizedTest
-    void doSearch(URI uri) {
+    @MethodSource("uriProvider")
+    void testWithExplicitLocalMethodSource(URI uri) {
         var response = swsOpenSearchClient.doSearch(uri);
         assertNotNull(response);
+    }
+
+    static Stream<URI> uriProvider() {
+        return Stream.of(
+            URI.create("https://example.com/?name=hello+world&lang=en"),
+            URI.create("https://example.com/?q=hello+world&lang=en"),
+            URI.create("https://example.com/?q=hello+world&lang=en"));
     }
 }
