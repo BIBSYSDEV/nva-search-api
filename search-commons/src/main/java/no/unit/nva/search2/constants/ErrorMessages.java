dbfedd3a5bee95bb985c4b97e34c3fefb08ccb93
Stig Norland <stig.norland@gmail.com>
2023-09-05 10:55:07 +0200

    java 17 build fail, fix?

--- search-commons/src/main/java/no/unit/nva/search2/constants/ErrorMessages.java
+++ search-commons/src/main/java/no/unit/nva/search2/constants/ErrorMessages.java
@@ -12,29 +12,23 @@ public class ErrorMessages {
     public static final String ERROR_MESSAGE_INVALID_VALUE = "Parameter '%s' has invalid value. ";
 
     public static final String ERROR_MESSAGE_INVALID_NUMBER = "Parameter '%s' has invalid value. Must be a number.";
-    public static final String ERROR_MESSAGE_INVALID_VALUE_WITH_RANGE =
-        "Parameter '%s' has invalid value. Supported values are: ";
-
     public static final String ERROR_MESSAGE_INVALID_DATE = "Parameter '%s' has invalid value. Must be a date.";
     public static final String ERROR_MESSAGE_PAGE_OUT_OF_SCOPE =
         "Page requested is out of scope. Query contains %s results";
     public static final String ERROR_MESSAGE_TEMPLATE_INVALID_QUERY_PARAMETERS =
         "Invalid query parameter supplied %s. Valid parameters: %s";
-
-    public static final String ERROR_MESSAGE_INVALID_PATH_PARAMETER = "Invalid path parameter for '%s'";
-    public static final String INVALID_URI_MESSAGE = "Must be valid URI";
     public static final String ERROR_MESSAGE_INVALID_FIELD_VALUE = "Invalid value for field '%s'";
 
 
     /**
      * Formats and emits a message with valid parameter names.
      *
-     * @param invalidkeys     list of invalid parameter names
+     * @param invalidKeys     list of invalid parameter names
      * @param queryParameters list of valid parameter names
      * @return formatted string containing a list of valid parameters
      */
-    public static String validQueryParameterNamesMessage(Set<String> invalidkeys,Set<String> queryParameters) {
-        return String.format(ERROR_MESSAGE_TEMPLATE_INVALID_QUERY_PARAMETERS, invalidkeys,
+    public static String validQueryParameterNamesMessage(Set<String> invalidKeys, Set<String> queryParameters) {
+        return String.format(ERROR_MESSAGE_TEMPLATE_INVALID_QUERY_PARAMETERS, invalidKeys,
                              prettifyList(queryParameters));
     }
 
@@ -49,36 +43,6 @@ public class ErrorMessages {
         return String.format(ERROR_MESSAGE_INVALID_VALUE + validValues, queryParameterName);
     }
 
-    /**
-     * Creates a error message containing which parameter that has invalid value and what the value is supposed to be.
-     *
-     * @param queryParameterName name of parameter with invalid value
-     * @param validValues        what values are allowed for this parameter
-     * @return formatted string containing a message with allowed values for this parameter
-     */
-    public static String invalidQueryParametersMessageWithRange(String queryParameterName, String validValues) {
-        return String.format(ERROR_MESSAGE_INVALID_VALUE_WITH_RANGE + validValues, queryParameterName);
-    }
-
-    /**
-     * Creates a error message containing which path parameter that has invalid value.
-     *
-     * @param pathParameterName name of parameter with invalid value
-     * @return formatted string containing a message with allowed values for this path parameter
-     */
-    public static String invalidPathParameterMessage(String pathParameterName) {
-        return String.format(ERROR_MESSAGE_INVALID_PATH_PARAMETER, pathParameterName);
-    }
-
-    /**
-     * Creates an error message containing which field that has invalid value.
-     *
-     * @param fieldParameterName name of field with invalid value
-     * @return formatted string containing a message with the field name containing invalid value
-     */
-    public static String invalidFieldParameterMessage(String fieldParameterName) {
-        return String.format(ERROR_MESSAGE_INVALID_FIELD_VALUE, fieldParameterName);
-    }
 
     public static String requiredMissingMessage(Set<String> missingKeys) {
         return String.format(ERROR_MESSAGE_INVALID_FIELD_VALUE, prettifyList(missingKeys));
