7b961d61ff018bd5e2ce622c30e2a9df5cd02eb2
Stig Norland <stig.norland@sikt.no>
2023-09-11 17:38:02 +0200

    fixed request url

--- search-commons/src/main/java/no/unit/nva/search2/SwsOpenSearchClient.java
+++ search-commons/src/main/java/no/unit/nva/search2/SwsOpenSearchClient.java
@@ -3,13 +3,16 @@ package no.unit.nva.search2;
 import no.unit.nva.auth.uriretriever.AuthorizedBackendUriRetriever;
 import no.unit.nva.auth.uriretriever.RawContentRetriever;
 import no.unit.nva.search2.model.OpenSearchSwsResponse;
+import nva.commons.apigateway.exceptions.BadGatewayException;
 import nva.commons.core.JacocoGenerated;
+import org.apache.http.HttpStatus;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
 import java.net.http.HttpResponse;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import static no.unit.nva.search2.constant.ApplicationConstants.SEARCH_INFRASTRUCTURE_CREDENTIALS;
@@ -21,7 +24,7 @@ import static org.apache.http.entity.ContentType.APPLICATION_JSON;
 public class SwsOpenSearchClient {
 
     private static final Logger logger = LoggerFactory.getLogger(SwsOpenSearchClient.class);
-    private static final String REQUESTING_SEARCH_FROM = "SwsOpenSearchClient search from {}";
+    private static final String REQUESTING_SEARCH_FROM = "SwsOpenSearchClient url -> {}";
     private final RawContentRetriever contentRetriever;
     private final String mediaType;
 
@@ -38,21 +41,13 @@ public class SwsOpenSearchClient {
         return new SwsOpenSearchClient(retriever, APPLICATION_JSON.toString());
     }
 
-    protected OpenSearchSwsResponse doSearch(URI requestUri) {
+    protected OpenSearchSwsResponse doSearch(URI requestUri) throws BadGatewayException {
         logger.info(REQUESTING_SEARCH_FROM, requestUri);
-        return
-            contentRetriever.fetchResponse(requestUri, mediaType)
-                .stream().peek(response -> logger.info("Response status: {}", response.statusCode()))
-                .peek(response -> logger.info("Response headers: {}", response.body()))
-                .map(toOpenSearchResponse())
-                .findFirst()
-                .orElseThrow();
+        var response = contentRetriever.fetchResponse(requestUri, mediaType).orElseThrow();
+        if (response.statusCode() != HttpStatus.SC_OK) {
+            throw new BadGatewayException(response.body());
+        }
+        return attempt(() -> objectMapperWithEmpty.readValue(response.body(), OpenSearchSwsResponse.class))
+            .orElseThrow();
     }
-
-    @NotNull
-    private Function<HttpResponse<String>, OpenSearchSwsResponse> toOpenSearchResponse() {
-        return response -> attempt(() -> objectMapperWithEmpty.readValue(response.body(), OpenSearchSwsResponse.class))
-                .orElseThrow();
-    }
-
 }
