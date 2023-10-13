a091a9397050c8c5fb7084dd0d668e6d961ec3e7
Kir Truhacev <kir.truhacev@sikt.no>
2023-10-13 15:30:28 +0200

    work in progress

--- batch-index/src/main/java/no/unit/nva/search/GenerateKeyBatchesHandler.java
+++ batch-index/src/main/java/no/unit/nva/search/GenerateKeyBatchesHandler.java
@@ -1,5 +1,6 @@
 package no.unit.nva.search;
 
+import static no.unit.nva.search.BatchIndexingConstants.defaultS3Client;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import java.io.InputStream;
@@ -7,21 +8,33 @@ import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import nva.commons.core.Environment;
+import nva.commons.core.JacocoGenerated;
+import nva.commons.core.paths.UriWrapper;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request.Builder;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Object;
 
-public class KeyBatchHandler implements RequestStreamHandler {
+public class GenerateKeyBatchesHandler implements RequestStreamHandler {
+
+    public static final String RESOURCE_DELIMITER = "/resource";
+    private static final Environment ENVIRONMENT = new Environment();
     private final S3Client inputClient;
     private final S3Client outputClient;
     private final String inputBucketName;
     private final String outputBucketName;
 
-    public KeyBatchHandler(S3Client inputClient, S3Client outputClient, String inputBucketName,
-                           String outputBucketName) {
+    @JacocoGenerated
+    public GenerateKeyBatchesHandler() {
+        this(defaultS3Client(), defaultS3Client(), ENVIRONMENT.readEnv("PERSISTED_RESOURCES_PATH"),
+             ENVIRONMENT.readEnv("KEY_BATCHES_BUCKET"));
+    }
+
+    public GenerateKeyBatchesHandler(S3Client inputClient, S3Client outputClient, String inputBucketName,
+                                     String outputBucketName) {
         this.inputClient = inputClient;
         this.outputClient = outputClient;
         this.inputBucketName = inputBucketName;
@@ -30,10 +43,15 @@ public class KeyBatchHandler implements RequestStreamHandler {
 
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) {
-        var request = ListObjectsV2Request.builder().bucket(inputBucketName).maxKeys(10).build();
-        inputClient.listObjectsV2Paginator(request).stream()
-                       .map(KeyBatchHandler::toKeySet)
-                       .forEach(this::writeObject);
+        var bucketName = UriWrapper.fromUri(inputBucketName).getHost().toString();
+        inputClient.listObjectsV2Paginator(request -> requestBuilder(request, bucketName))
+            .stream()
+            .map(GenerateKeyBatchesHandler::toKeySet)
+            .forEach(this::writeObject);
+    }
+
+    private static void requestBuilder(Builder request, String bucketName) {
+        request.bucket(bucketName).delimiter(RESOURCE_DELIMITER).maxKeys(10);
     }
 
     private static String toKeySet(ListObjectsV2Response item) {
--- batch-index/src/test/java/no/unit/nva/search/GenerateKeyBatchesHandlerTest.java
+++ batch-index/src/test/java/no/unit/nva/search/GenerateKeyBatchesHandlerTest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import java.io.ByteArrayOutputStream;
 import java.net.URI;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -20,10 +21,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
 
-class KeyBatchHandlerTest {
+class GenerateKeyBatchesHandlerTest {
 
     public static final String OUTPUT_BUCKET = "outputBucket";
-    public static final String INPUT_BUCKET = "inputBucket";
+    public static final String INPUT_BUCKET_PATH = "s3://inputBucket/resources";
+    public static final String RESOURCES = "resources";
     private S3Driver s3DriverInputBucket;
     private S3Driver s3DriverOutputBucket;
     private FakeS3Client outputClient;
@@ -32,28 +34,38 @@ class KeyBatchHandlerTest {
     @BeforeEach
     void setUp() {
         s3ClientInputClient = new FakeS3Client();
-        s3DriverInputBucket = new S3Driver(s3ClientInputClient, INPUT_BUCKET);
+        s3DriverInputBucket = new S3Driver(s3ClientInputClient, INPUT_BUCKET_PATH);
         outputClient = new FakeS3Client();
         s3DriverOutputBucket = new S3Driver(outputClient, OUTPUT_BUCKET);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {100})
+    @ValueSource(ints = {0, 1, 10, 25, 100, 136})
     void shouldReadS3KeysFromPersistedBucketAndWriteToS3BatchBucket(int numberOfItemsInBucket) {
         final var expected = putObjectsInInputBucket(numberOfItemsInBucket);
 
-        var handler = new KeyBatchHandler(s3ClientInputClient, outputClient, INPUT_BUCKET, OUTPUT_BUCKET);
+        var handler = new GenerateKeyBatchesHandler(s3ClientInputClient, outputClient, INPUT_BUCKET_PATH,
+                                                    OUTPUT_BUCKET);
         handler.handleRequest(null, new ByteArrayOutputStream(), new FakeContext());
 
         var actual = getPersistedFileFromOutputBucket();
-        var expectedNumberOfFiles = numberOfItemsInBucket < 11 ? 1 : numberOfItemsInBucket / 10;
+        var expectedNumberOfFiles = getExpectedNumberOfFiles(numberOfItemsInBucket);
 
         assertThat(actual.size(), is(equalTo(expectedNumberOfFiles)));
         assertThat(actual.stream().collect(Collectors.joining(System.lineSeparator())), is(equalTo(expected)));
     }
 
+    private static int getExpectedNumberOfFiles(int numberOfItemsInBucket) {
+        return (int) Math.ceil(numberOfItemsInBucket < 11 ? 1 : (double) numberOfItemsInBucket / 10);
+    }
+
+    private static String getBucketPath(UriWrapper uri) {
+        return Path.of(UnixPath.fromString(uri.toString()).getPathElementByIndexFromEnd(1), uri.getLastPathElement())
+                   .toString();
+    }
+
     private List<String> getPersistedFileFromOutputBucket() {
-        var request = ListObjectsV2Request.builder().bucket(OUTPUT_BUCKET).maxKeys(10).build();
+        var request = ListObjectsV2Request.builder().bucket(OUTPUT_BUCKET).maxKeys(10_000).build();
         var content = outputClient.listObjectsV2(request).contents();
         return content.stream().map(object -> s3DriverOutputBucket.getFile(UnixPath.of(object.key()))).toList();
     }
@@ -63,11 +75,12 @@ class KeyBatchHandlerTest {
                    .mapToObj(item -> randomString())
                    .map(this::insertFileWithKey)
                    .map(UriWrapper::fromUri)
-                   .map(UriWrapper::getLastPathElement)
+                   .map(GenerateKeyBatchesHandlerTest::getBucketPath)
                    .collect(Collectors.joining(System.lineSeparator()));
     }
 
     private URI insertFileWithKey(String key) {
-        return attempt(() -> s3DriverInputBucket.insertFile(UnixPath.of(key), randomString())).orElseThrow();
+        return attempt(
+            () -> s3DriverInputBucket.insertFile(UnixPath.of(RESOURCES, key), randomString())).orElseThrow();
     }
 }
