f0dd6876542c767d6f77a18a14de3ad91a912cb3
Kir Truhacev <kir.truhacev@sikt.no>
2023-10-18 23:06:41 +0200

    switcing to eventbridge

--- batch-index/src/main/java/no/unit/nva/search/keybatch/StartKeyBasedBatchHandler.java
+++ batch-index/src/main/java/no/unit/nva/search/keybatch/StartKeyBasedBatchHandler.java
@@ -1,10 +1,13 @@
 package no.unit.nva.search.keybatch;
 
+import static no.unit.nva.search.EmitEventUtils.INDICATION_THAT_EVENT_TYPE_IS_INSIDE_DETAIL;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.time.Instant;
+import no.unit.nva.search.EventBasedBatchIndexer;
 import nva.commons.core.Environment;
 import nva.commons.core.JacocoGenerated;
 import org.slf4j.Logger;
@@ -33,13 +36,20 @@ public class StartKeyBasedBatchHandler implements RequestStreamHandler {
 
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
-        var event = constructRequestEntry();
+        var event = constructRequestEntry(context);
         var response = sendEvent(event);
         logger.info(response.toString());
     }
 
-    private static PutEventsRequestEntry constructRequestEntry() {
-        return PutEventsRequestEntry.builder().eventBusName(EVENT_BUS).detail(KEY_BASED_BATCH).build();
+    private static PutEventsRequestEntry constructRequestEntry(Context context) {
+        return PutEventsRequestEntry.builder()
+                   .eventBusName(EVENT_BUS)
+                   .detail(KEY_BASED_BATCH)
+                   .detailType(INDICATION_THAT_EVENT_TYPE_IS_INSIDE_DETAIL)
+                   .source(EventBasedBatchIndexer.class.getName())
+                   .resources(context.getInvokedFunctionArn())
+                   .time(Instant.now())
+                   .build();
     }
 
     @JacocoGenerated
