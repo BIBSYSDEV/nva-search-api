33b76c5d9128713d0af0b4a5338885f1157bebe4
Kir Truhacev <kir.truhacev@sikt.no>
2023-10-18 23:10:58 +0200

    switcing to eventbridge

--- batch-index/src/test/java/no/unit/nva/search/keybatch/StartKeyBasedBatchHandlerTest.java
+++ batch-index/src/test/java/no/unit/nva/search/keybatch/StartKeyBasedBatchHandlerTest.java
@@ -4,9 +4,9 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.mock;
+import com.amazonaws.services.lambda.runtime.Context;
 import java.io.IOException;
-import no.unit.nva.search.keybatch.StartKeyBasedBatchHandler;
-import no.unit.nva.stubs.FakeContext;
 import no.unit.nva.stubs.FakeEventBridgeClient;
 import nva.commons.core.Environment;
 import org.junit.jupiter.api.Test;
@@ -17,7 +17,7 @@ class StartKeyBasedBatchHandlerTest {
     void shouldSendEventWithTopic() throws IOException {
         var client = new FakeEventBridgeClient();
         var handler = new StartKeyBasedBatchHandler(client);
-        handler.handleRequest(null, null, new FakeContext());
+        handler.handleRequest(null, null, mock(Context.class));
         var eventDetail = client.getRequestEntries().get(0).detail();
 
         assertThat(client.getRequestEntries(), hasSize(1));
