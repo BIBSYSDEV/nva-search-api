329e51ecda7c75740d0a853059e8cba23119844d
Stig Norland <stig.norland@sikt.no>
2023-10-20 14:17:12 +0200

    https://unit.atlassian.net/browse/NP-45477

--- search-commons/src/main/java/no/unit/nva/search2/constant/Patterns.java
+++ search-commons/src/main/java/no/unit/nva/search2/constant/Patterns.java
@@ -5,7 +5,9 @@ import nva.commons.core.JacocoGenerated;
 @JacocoGenerated
 public class Patterns {
     public static final String PATTERN_IS_DATE = "(\\d){4}-(\\d){2}-(\\d){2}[T]*[(\\d){2}:(\\d){2}:(\\d){2,6}Z]*";
+        // yyyy-MM-dd | yyyy-MM-ddTHH:mm:ssZ | yyyy-MM-ddTHH:mm:ss.SSSZ
     public static final String PATTERN_IS_DATE_STRING = "\\b\\d{4}(?:-\\d{2}-\\d{2})?\\b";
+        // yyyy | yyyy-MM-dd
     public static final String PATTERN_IS_NON_EMPTY = ".+";
     public static final String PATTERN_IS_NUMBER = "[0-9]\\d*";
     public static final String PATTERN_IS_NONE_OR_ONE = ".?";
