a12ddbb57c3cb61401a7c870a952287c0104a6e0
Stig Norland <stig.norland@gmail.com>
2023-11-01 14:20:19 +0100

    year.?reported publication.?year FUNDING -> name : ID

--- search-commons/src/main/java/no/unit/nva/search2/constant/Patterns.java
+++ search-commons/src/main/java/no/unit/nva/search2/constant/Patterns.java
@@ -1,5 +1,6 @@
 package no.unit.nva.search2.constant;
 
+import static no.unit.nva.search2.constant.ApplicationConstants.COLON;
 import nva.commons.core.JacocoGenerated;
 
 @JacocoGenerated
@@ -16,13 +17,17 @@ public class Patterns {
      *  yyyy | yyyy-MM-dd
      */
     public static final String PATTERN_IS_DATE_STRING = "\\b\\d{4}(?:-\\d{2}-\\d{2})?\\b";
-
-
     public static final String PATTERN_IS_ADD_SLASH = "\\\\$1";
     public static final String PATTERN_IS_IGNORE_CASE = "(?i)";
     public static final String PATTERN_IS_NONE_OR_ONE = ".?";
     public static final String PATTERN_IS_NON_EMPTY = ".+";
     public static final String PATTERN_IS_NUMBER = "[0-9]\\d*";
+
+    /**
+     * Pattern for matching a funding string.
+     * funding source and project_id together separated by ':'
+     */
+    public static final String PATTERN_IS_FUNDING =  PATTERN_IS_NON_EMPTY + COLON + PATTERN_IS_NON_EMPTY;
 
     /**
      * Pattern for matching group of opensearch special characters.
--- search-commons/src/main/java/no/unit/nva/search2/model/ResourceParameterKey.java
+++ search-commons/src/main/java/no/unit/nva/search2/model/ResourceParameterKey.java
@@ -19,6 +19,7 @@ import static no.unit.nva.search2.constant.ErrorMessages.INVALID_NUMBER;
 import static no.unit.nva.search2.constant.ErrorMessages.INVALID_VALUE;
 import static no.unit.nva.search2.constant.ErrorMessages.INVALID_VALUE_WITH_SORT;
 import static no.unit.nva.search2.constant.Patterns.PATTERN_IS_DATE_STRING;
+import static no.unit.nva.search2.constant.Patterns.PATTERN_IS_FUNDING;
 import static no.unit.nva.search2.constant.Patterns.PATTERN_IS_IGNORE_CASE;
 import static no.unit.nva.search2.constant.Patterns.PATTERN_IS_DATE;
 import static no.unit.nva.search2.constant.Patterns.PATTERN_IS_NONE_OR_ONE;
@@ -34,6 +35,13 @@ import static no.unit.nva.search2.model.ParameterKey.ParamKind.NUMBER;
 import static no.unit.nva.search2.model.ParameterKey.ParamKind.SORT_STRING;
 import static no.unit.nva.search2.model.ParameterKey.ParamKind.STRING;
 
+/**
+ * Enum for all the parameters that can be used to query the search index.
+ * This enum needs to implement these parameters
+ * <a href="https://api.cristin.no/v2/doc/index.html#GETresults">cristin API</a>
+ *
+ */
+
 public enum ResourceParameterKey implements ParameterKey {
     INVALID(STRING),
     // Parameters converted to Lucene query
@@ -42,7 +50,8 @@ public enum ResourceParameterKey implements ParameterKey {
     CREATED_BEFORE(DATE, LESS_THAN, "createdDate"),
     CREATED_SINCE(DATE, GREATER_THAN_OR_EQUAL_TO, "createdDate"),
     DOI(STRING, "entityDescription.reference.doi"),
-    FUNDING(STRING, "fundings.identifier"),
+    FUNDING(CUSTOM, EQUALS, "projects.id|fundings.source.labels",
+            null, PATTERN_IS_FUNDING,null),
     FUNDING_SOURCE(STRING, "fundings.source.identifier|fundings.source.labels"),
     ID(STRING, "identifier"),
     INSTITUTION(STRING, "entityDescription.contributors.affiliation"
@@ -59,7 +68,8 @@ public enum ResourceParameterKey implements ParameterKey {
     TITLE(STRING, "entityDescription.mainTitle", 2F),
     UNIT(STRING, "entityDescription.contributors.affiliation.id"),
     USER(STRING, "resourceOwner.owner"),
-    PUBLICATION_YEAR(NUMBER, "entityDescription.publicationDate.year"),
+    PUBLICATION_YEAR(NUMBER, EQUALS, "entityDescription.publicationDate.year",
+                     "(?i)year.?reported|publication.?year", null, null),
     // Query parameters passed to SWS/Opensearch
     SEARCH_ALL(STRING, EQUALS, "q", "(?i)search.?all|query", null, null),
     FIELDS(STRING),
