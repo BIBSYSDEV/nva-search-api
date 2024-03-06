113760759bd0054157b278dbb798226628d4edd4
Stig Norland <stig.norland@sikt.no>
2024-03-06 15:45:01 +0100

    added filter for document.type

--- search-resources-api/src/main/java/no/unit/nva/search2/SearchTicketAuthHandler.java
+++ search-resources-api/src/main/java/no/unit/nva/search2/SearchTicketAuthHandler.java
@@ -1,18 +1,22 @@
 package no.unit.nva.search2;
 
 import static no.unit.nva.search2.common.constant.Defaults.DEFAULT_RESPONSE_MEDIA_TYPES;
-import static no.unit.nva.search2.common.enums.TicketStatus.COMPLETED;
-import static no.unit.nva.search2.common.enums.TicketStatus.NEW;
 import static no.unit.nva.search2.ticket.TicketClient.defaultClient;
 import static no.unit.nva.search2.ticket.TicketParameter.AGGREGATION;
 import static no.unit.nva.search2.ticket.TicketParameter.FROM;
 import static no.unit.nva.search2.ticket.TicketParameter.SIZE;
+import static nva.commons.apigateway.AccessRight.MANAGE_DOI;
+import static nva.commons.apigateway.AccessRight.MANAGE_PUBLISHING_REQUESTS;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.google.common.net.MediaType;
 import java.net.HttpURLConnection;
+import java.util.HashSet;
 import java.util.List;
+
 import no.unit.nva.search2.ticket.TicketClient;
 import no.unit.nva.search2.ticket.TicketQuery;
+import no.unit.nva.search2.ticket.TicketType;
 import nva.commons.apigateway.AccessRight;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
@@ -39,7 +43,7 @@ public class SearchTicketAuthHandler extends ApiGatewayHandler<Void, String> {
     protected String processInput(Void input, RequestInfo requestInfo, Context context)
         throws BadRequestException, UnauthorizedException {
 
-        validateAccessRight(requestInfo);
+        var ticketTypes = validateAccessRight(requestInfo);
 
         return
             TicketQuery.builder()
@@ -47,8 +51,7 @@ public class SearchTicketAuthHandler extends ApiGatewayHandler<Void, String> {
                 .withRequiredParameters(FROM, SIZE, AGGREGATION)
                 .validate()
                 .build()
-                .withRequiredStatus(NEW, COMPLETED)
-                .withOrganization(requestInfo.getCurrentCustomer())
+                .withRequiredTypeFilter(ticketTypes)
                 .doSearch(opensearchClient);
     }
 
@@ -62,12 +65,24 @@ public class SearchTicketAuthHandler extends ApiGatewayHandler<Void, String> {
         return DEFAULT_RESPONSE_MEDIA_TYPES;
     }
 
-    private void validateAccessRight(RequestInfo requestInfo) throws UnauthorizedException {
-        if (requestInfo.userIsAuthorized(AccessRight.MANAGE_OWN_AFFILIATION)
-            || requestInfo.userIsAuthorized(AccessRight.MANAGE_RESOURCES_STANDARD)) {
-            return;
+    private TicketType[] validateAccessRight(RequestInfo requestInfo) throws UnauthorizedException {
+        var allowed = new HashSet<TicketType>();
+        if (requestInfo.userIsAuthorized(MANAGE_DOI)) {
+            allowed.add(TicketType.DOI_REQUEST);
         }
-
-        throw new UnauthorizedException();
+        if (requestInfo.userIsAuthorized(AccessRight.SUPPORT)) {
+            allowed.add(TicketType.GENERAL_SUPPORT_CASE);
+        }
+        if (requestInfo.userIsAuthorized(MANAGE_PUBLISHING_REQUESTS)) {
+            allowed.add(TicketType.PUBLISHING_REQUEST);
+        }
+        if (allowed.isEmpty()) {
+            throw new UnauthorizedException();
+        }
+        return allowed.toArray(TicketType[]::new);
     }
+
+
+
+
 }
