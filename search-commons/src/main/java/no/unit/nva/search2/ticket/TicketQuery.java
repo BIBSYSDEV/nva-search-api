84c8c3359e54d7c79db8b8583ea68c1df2cc0008
Stig Norland <stig.norland@gmail.com>
2024-04-04 13:59:25 +0200

    Np 46301/np 46526 type check type (#478)
    * add type check
    * added toEnumStrings

--- search-commons/src/main/java/no/unit/nva/search2/ticket/TicketQuery.java
+++ search-commons/src/main/java/no/unit/nva/search2/ticket/TicketQuery.java
@@ -1,7 +1,6 @@
 package no.unit.nva.search2.ticket;
 
 import static no.unit.nva.search2.common.QueryTools.decodeUTF;
-import no.unit.nva.search2.common.builder.OpensearchQueryFuzzyKeyword;
 import static no.unit.nva.search2.common.constant.Defaults.DEFAULT_OFFSET;
 import static no.unit.nva.search2.common.constant.Defaults.DEFAULT_SORT_ORDER;
 import static no.unit.nva.search2.common.constant.Defaults.DEFAULT_VALUE_PER_PAGE;
@@ -17,7 +16,6 @@ import static no.unit.nva.search2.common.constant.Words.NONE;
 import static no.unit.nva.search2.common.constant.Words.POST_FILTER;
 import static no.unit.nva.search2.common.constant.Words.SEARCH;
 import static no.unit.nva.search2.common.constant.Words.TICKETS;
-import static no.unit.nva.search2.ticket.TicketStatus.PENDING;
 import static no.unit.nva.search2.ticket.Constants.DEFAULT_TICKET_SORT;
 import static no.unit.nva.search2.ticket.Constants.NOTIFICATION;
 import static no.unit.nva.search2.ticket.Constants.ORGANIZATION_ID_KEYWORD;
@@ -40,6 +38,7 @@ import static no.unit.nva.search2.ticket.TicketParameter.SORT;
 import static no.unit.nva.search2.ticket.TicketParameter.SORT_ORDER;
 import static no.unit.nva.search2.ticket.TicketParameter.STATUS;
 import static no.unit.nva.search2.ticket.TicketParameter.TICKET_PARAMETER_SET;
+import static no.unit.nva.search2.ticket.TicketStatus.PENDING;
 import static nva.commons.apigateway.AccessRight.MANAGE_DOI;
 import static nva.commons.apigateway.AccessRight.MANAGE_PUBLISHING_REQUESTS;
 import static nva.commons.core.StringUtils.EMPTY_STRING;
@@ -58,6 +57,7 @@ import java.util.stream.Stream;
 import no.unit.nva.search2.common.AsType;
 import no.unit.nva.search2.common.ParameterValidator;
 import no.unit.nva.search2.common.Query;
+import no.unit.nva.search2.common.builder.OpensearchQueryFuzzyKeyword;
 import no.unit.nva.search2.common.builder.OpensearchQueryText;
 import no.unit.nva.search2.common.enums.SortKey;
 import no.unit.nva.search2.common.enums.ValueEncoding;
@@ -382,6 +382,7 @@ public final class TicketQuery extends Query<TicketParameter> {
                 case SORT_ORDER -> mergeToKey(SORT, decodedValue);
                 case CREATED_DATE, MODIFIED_DATE, PUBLICATION_MODIFIED_DATE ->
                     query.parameters().set(qpKey, expandYearToDate(decodedValue));
+                case TYPE -> mergeToKey(qpKey, toEnumStrings(TicketType::fromString, decodedValue));
                 case STATUS -> mergeToKey(qpKey, toEnumStrings(TicketStatus::fromString, decodedValue));
                 default -> mergeToKey(qpKey, decodedValue);
             }
