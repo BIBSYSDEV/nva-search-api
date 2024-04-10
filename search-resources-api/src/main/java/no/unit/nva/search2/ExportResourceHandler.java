c42d676b7d2d1bf5dc8564d72cd314a2256bacee
Sondre Vestad <sonves@gmail.com>
2024-04-10 15:12:37 +0200

    wip

---
+++ search-resources-api/src/main/java/no/unit/nva/search2/ExportResourceHandler.java
@@ -0,0 +1,57 @@
+package no.unit.nva.search2;
+
+import static no.unit.nva.search2.common.constant.Defaults.DEFAULT_RESPONSE_MEDIA_TYPES;
+import static no.unit.nva.search2.common.enums.PublicationStatus.PUBLISHED;
+import static no.unit.nva.search2.common.enums.PublicationStatus.PUBLISHED_METADATA;
+import static no.unit.nva.search2.resource.ResourceClient.defaultClient;
+import static no.unit.nva.search2.resource.ResourceParameter.AGGREGATION;
+import static no.unit.nva.search2.resource.ResourceParameter.FROM;
+import static no.unit.nva.search2.resource.ResourceParameter.SIZE;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.google.common.net.MediaType;
+import java.net.HttpURLConnection;
+import java.util.List;
+import no.unit.nva.search2.resource.ResourceClient;
+import no.unit.nva.search2.resource.ResourceQuery;
+import nva.commons.apigateway.ApiGatewayHandler;
+import nva.commons.apigateway.RequestInfo;
+import nva.commons.apigateway.exceptions.BadRequestException;
+import nva.commons.core.Environment;
+import nva.commons.core.JacocoGenerated;
+
+public class ExportResourceHandler extends ApiGatewayHandler<Void, String> {
+
+    private final ResourceClient opensearchClient;
+
+    @JacocoGenerated
+    public ExportResourceHandler() {
+        this(new Environment(), defaultClient());
+    }
+
+    public ExportResourceHandler(Environment environment, ResourceClient resourceClient) {
+        super(Void.class, environment);
+        this.opensearchClient = resourceClient;
+    }
+
+    @Override
+    protected String processInput(Void input, RequestInfo requestInfo, Context context) throws BadRequestException {
+        return
+            ResourceQuery.builder()
+                .fromRequestInfo(requestInfo)
+                .withRequiredParameters(FROM, SIZE, AGGREGATION)
+                .validate()
+                .build()
+                .withRequiredStatus(PUBLISHED, PUBLISHED_METADATA)
+                .doSearch(opensearchClient);
+    }
+
+    @Override
+    protected Integer getSuccessStatusCode(Void input, String output) {
+        return HttpURLConnection.HTTP_OK;
+    }
+
+    @Override
+    protected List<MediaType> listSupportedMediaTypes() {
+        return DEFAULT_RESPONSE_MEDIA_TYPES;
+    }
+}
