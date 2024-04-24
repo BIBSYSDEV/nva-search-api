f914f3c7e0b52758a295b495f480d1e5ad4762f4
Stig Norland <stig.norland@sikt.no>
2024-04-24 13:44:41 +0200

    fixed aggregations

--- search-commons/src/test/java/no/unit/nva/search2/common/OpensearchQueryToolsTest.java
+++ search-commons/src/test/java/no/unit/nva/search2/common/OpensearchQueryToolsTest.java
@@ -58,7 +58,6 @@ class OpensearchQueryToolsTest {
     }
 
     @Test
-    @Disabled
     void printImportCandidateParameter() {
         printEnum(Arrays.stream(ImportCandidateParameter.values()));
     }
