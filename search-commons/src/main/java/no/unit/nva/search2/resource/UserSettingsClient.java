08da3dde135fde49a6e7dc5c4412e4670fbac2ea
Stig Norland <stig.norland@sikt.no>
2024-05-03 14:05:04 +0200

    merge main

--- search-commons/src/main/java/no/unit/nva/search2/resource/UserSettingsClient.java
+++ search-commons/src/main/java/no/unit/nva/search2/resource/UserSettingsClient.java
@@ -3,8 +3,8 @@ package no.unit.nva.search2.resource;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static no.unit.nva.auth.AuthorizedBackendClient.AUTHORIZATION_HEADER;
 import static no.unit.nva.auth.AuthorizedBackendClient.CONTENT_TYPE;
+import static no.unit.nva.auth.uriretriever.UriRetriever.ACCEPT;
 import static no.unit.nva.commons.json.JsonUtils.singleLineObjectMapper;
-import static no.unit.nva.search.utils.UriRetriever.ACCEPT;
 import static no.unit.nva.search2.common.constant.Functions.readApiHost;
 import static no.unit.nva.search2.common.constant.Words.HTTPS;
 import static no.unit.nva.search2.resource.Constants.PERSON_PREFERENCES;
@@ -22,9 +22,9 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import no.unit.nva.commons.json.JsonSerializable;
-import no.unit.nva.search.CachedJwtProvider;
 import no.unit.nva.search2.common.OpenSearchClient;
 import no.unit.nva.search2.common.records.UserSettings;
+import no.unit.nva.search2.common.security.CachedJwtProvider;
 import nva.commons.core.attempt.FunctionWithException;
 
 public class UserSettingsClient extends OpenSearchClient<UserSettings, ResourceSearchQuery> {
