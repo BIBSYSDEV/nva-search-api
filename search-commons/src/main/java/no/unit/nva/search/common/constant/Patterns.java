458e502cb8a9fd90df505d5f537f1c4be61c11f5
Stig Norland <stig.norland@sikt.no>
2024-08-28 14:02:37 +0200

    qa feedback

--- search-commons/src/main/java/no/unit/nva/search/common/constant/Patterns.java
+++ search-commons/src/main/java/no/unit/nva/search/common/constant/Patterns.java
@@ -41,7 +41,7 @@ public final class Patterns {
     public static final String PATTERN_IS_SIZE_KEY = "(?i)per_?page|results|limit|size";
     public static final String PATTERN_IS_SORT_KEY = "(?i)order_?by|sort";
     public static final String PATTERN_IS_SORT_ORDER_KEY = "(?i)sort_?order|order";
-    public static final String PATTERN_IS_URI = "https?://[^\\s/$.?#].[^\\s]*"; // Pattern for matching a URI
+    public static final String PATTERN_IS_URI = "https?://[^\\s/$.?#].[^\\s]*";
     public static final String PATTERN_IS_URL_PARAM_INDICATOR = "\\?";
     public static final String PATTERN_IS_PIPE = "\\|";
 
