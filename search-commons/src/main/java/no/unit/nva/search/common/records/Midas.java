2864ee0c11bd1c3969de414b36c3c6a345d6d8eb
Stig Norland <stig.norland@sikt.no>
2024-09-11 13:53:14 +0200

    QA feedback

--- search-commons/src/main/java/no/unit/nva/search/common/records/Midas.java
+++ search-commons/src/main/java/no/unit/nva/search/common/records/Midas.java
@@ -2,6 +2,9 @@ package no.unit.nva.search.common.records;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+/**
+ * Midas was a mythological king who transmuted everything he touched into gold.
+ */
 public interface Midas {
     JsonNode transform(JsonNode source);
 }
