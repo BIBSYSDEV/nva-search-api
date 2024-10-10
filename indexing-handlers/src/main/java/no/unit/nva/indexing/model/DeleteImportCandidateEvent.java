fbafd645a42e985417e2d5a4ba2442908a2de4de
Stig Norland <stig.norland@sikt.no>
2024-10-10 16:29:17 +0200

    Moved SHARD_ID to DEFAULT_SHARD_ID

--- indexing-handlers/src/main/java/no/unit/nva/indexing/model/DeleteImportCandidateEvent.java
+++ indexing-handlers/src/main/java/no/unit/nva/indexing/model/DeleteImportCandidateEvent.java
@@ -6,18 +6,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import no.unit.nva.commons.json.JsonSerializable;
 import no.unit.nva.identifiers.SortableIdentifier;
 
-import nva.commons.core.JacocoGenerated;
-
-import java.util.Objects;
-
-public class DeleteImportCandidateEvent implements JsonSerializable {
+public record DeleteImportCandidateEvent(String topic, SortableIdentifier identifier) implements JsonSerializable {
 
     public static final String EVENT_TOPIC = "ImportCandidates.ExpandedEntry.Deleted";
     public static final String TOPIC = "topic";
     public static final String IDENTIFIER = "identifier";
-
-    private final String topic;
-    private final SortableIdentifier identifier;
 
     @JsonCreator
     public DeleteImportCandidateEvent(
@@ -25,33 +18,5 @@ public class DeleteImportCandidateEvent implements JsonSerializable {
             @JsonProperty(IDENTIFIER) SortableIdentifier identifier) {
         this.topic = topic;
         this.identifier = identifier;
-    }
-
-    @JacocoGenerated
-    public String getTopic() {
-        return topic;
-    }
-
-    public SortableIdentifier getIdentifier() {
-        return identifier;
-    }
-
-    @JacocoGenerated
-    @Override
-    public int hashCode() {
-        return Objects.hash(getTopic(), getIdentifier());
-    }
-
-    @Override
-    @JacocoGenerated
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        DeleteImportCandidateEvent that = (DeleteImportCandidateEvent) o;
-        return topic.equals(that.topic) && identifier.equals(that.identifier);
     }
 }
