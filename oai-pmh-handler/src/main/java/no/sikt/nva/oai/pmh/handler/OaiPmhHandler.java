9972369d43896e1dc943fcba3adde8c1a2c43f6d
Ketil Aasarød <55836988+ketilaa@users.noreply.github.com>
2025-01-20 16:09:32 +0100

    cfn for oai-pmh handler (#635)
    * cfn for oai-pmh handler
    
    * removed commented out code and example
    
    * added lambda integration for POST method
    
    * now uses implicit permissions for lambda
    
    * added no-arg constructor for handler
    
    * environment for no-arg constructor
    
    * commented out oai-pmf to check deploy issue
    
    * commented oai-pmf back in
    
    * base path mapping for publication OAI-PMH Apu Gateway

--- oai-pmh-handler/src/main/java/no/sikt/nva/oai/pmh/handler/OaiPmhHandler.java
+++ oai-pmh-handler/src/main/java/no/sikt/nva/oai/pmh/handler/OaiPmhHandler.java
@@ -2,16 +2,30 @@ package no.sikt.nva.oai.pmh.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.google.common.net.MediaType;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
 import java.net.HttpURLConnection;
 import java.util.List;
 import nva.commons.apigateway.ApiGatewayHandler;
 import nva.commons.apigateway.RequestInfo;
 import nva.commons.apigateway.exceptions.ApiGatewayException;
 import nva.commons.core.Environment;
+import nva.commons.core.JacocoGenerated;
+import org.openarchives.oai.pmh.v2.OAIPMHtype;
 
 public class OaiPmhHandler extends ApiGatewayHandler<Void, String> {
   private final XmlSerializer xmlSerializer;
   private final OaiPmhDataProvider dataProvider = new DefaultOaiPmhDataProvider();
+
+  @JacocoGenerated
+  public OaiPmhHandler() throws JAXBException {
+    super(Void.class, new Environment());
+
+    var context = JAXBContext.newInstance(OAIPMHtype.class);
+    var marshaller = context.createMarshaller();
+
+    this.xmlSerializer = new JaxbXmlSerializer(marshaller);
+  }
 
   public OaiPmhHandler(Environment environment, XmlSerializer xmlSerializer) {
     super(Void.class, environment);
