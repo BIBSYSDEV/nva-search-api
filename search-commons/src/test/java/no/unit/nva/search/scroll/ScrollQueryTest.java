78122b64339285736406d1909a8adf72d61f1cb9
Ketil Aasarød <ketil.aasarod@sikt.no>
2025-02-20 15:49:24 +0100

    fixed test coverage

--- search-commons/src/test/java/no/unit/nva/search/scroll/ScrollQueryTest.java
+++ search-commons/src/test/java/no/unit/nva/search/scroll/ScrollQueryTest.java
@@ -1,5 +1,6 @@
 package no.unit.nva.search.scroll;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
@@ -8,10 +9,15 @@ public class ScrollQueryTest {
   @Test
   void shouldGenerateSensibleQuery() {
     var scrollId = "myScrollId";
-    var query = ScrollQuery.builder().withTtl("1m").withScrollId(scrollId).build();
+    var ttl = "1m";
+    var query = ScrollQuery.builder().withTtl(ttl).withScrollId(scrollId).build();
 
     var scrollClient = mock(ScrollClient.class);
 
+    query.assemble();
     query.doSearch(scrollClient);
+
+    assertEquals(ttl, query.getTtl());
+    assertEquals(scrollId, query.getScrollId());
   }
 }
