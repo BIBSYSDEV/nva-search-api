329c928948eeda93fa992019fdc8a37103500c29
Ketil Aasarød <55836988+ketilaa@users.noreply.github.com>
2025-10-03 13:53:33 +0200

    now handles organizations without labels properly (#717)

--- search-commons/src/main/java/no/unit/nva/search/resource/response/Organization.java
+++ search-commons/src/main/java/no/unit/nva/search/resource/response/Organization.java
@@ -1,13 +1,20 @@
 package no.unit.nva.search.resource.response;
 
+import static java.util.Objects.nonNull;
 import static nva.commons.core.attempt.Try.attempt;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import java.net.URI;
+import java.util.Collections;
 import java.util.Map;
 import no.unit.nva.commons.json.JsonUtils;
 
 public record Organization(URI id, Map<String, String> labels) {
+
+  public Organization(URI id, Map<String, String> labels) {
+    this.id = id;
+    this.labels = nonNull(labels) ? labels : Collections.emptyMap();
+  }
 
   public static Organization fromJsonNode(JsonNode node) {
     return attempt(() -> JsonUtils.dtoObjectMapper.treeToValue(node, Organization.class))
