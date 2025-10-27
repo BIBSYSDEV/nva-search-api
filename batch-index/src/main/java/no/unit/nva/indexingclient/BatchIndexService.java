8a17491ed7bc89e6766b08d420c25be595f6c8bd
Kir Truhacev <kir.truhacev@sikt.no>
2025-10-27 13:09:29 +0100

    work in progress

--- batch-index/src/main/java/no/unit/nva/indexingclient/BatchIndexService.java
+++ batch-index/src/main/java/no/unit/nva/indexingclient/BatchIndexService.java
@@ -2,7 +2,6 @@ package no.unit.nva.indexingclient;
 
 import static no.unit.nva.constants.Defaults.ENVIRONMENT;
 import static nva.commons.core.attempt.Try.attempt;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -29,128 +28,129 @@ import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 public class BatchIndexService {
 
-  public static final String FAILED_DOCUMENT_MESSAGE = "Failed to index document: {} at {}";
-  public static final String FAILED_TO_FETCH_FROM_S3_MESSAGE =
-      "Failed to fetch IndexDocument from S3 for event " + "reference: {}";
-  public static final String FAILED_TO_PARSE_SQS_MESSAGE = "Failed to parse SQS message: {} {}";
-  public static final String DLQ_QUEUE = "DLQ_QUEUE";
-  private static final Logger LOGGER = LoggerFactory.getLogger(BatchIndexService.class);
-  private static final String PERSISTED_RESOURCES_BUCKET =
-      ENVIRONMENT.readEnv("PERSISTED_RESOURCES_BUCKET");
+    public static final String FAILED_DOCUMENT_MESSAGE = "Failed to index document: {} at {}";
+    public static final String FAILED_TO_FETCH_FROM_S3_MESSAGE = "Failed to fetch IndexDocument from S3 for event "
+                                                                 + "reference: {}";
+    public static final String FAILED_TO_PARSE_SQS_MESSAGE = "Failed to parse SQS message: {} {}";
+    public static final String DLQ_QUEUE = "DLQ_QUEUE";
+    private static final Logger LOGGER = LoggerFactory.getLogger(BatchIndexService.class);
+    private static final String PERSISTED_RESOURCES_BUCKET = ENVIRONMENT.readEnv("PERSISTED_RESOURCES_BUCKET");
 
-  private final S3Driver s3Driver;
-  private final IndexingClient indexingClient;
-  private final QueueClient queueClient;
+    private final S3Driver s3Driver;
+    private final IndexingClient indexingClient;
+    private final QueueClient queueClient;
+    private final HashMap<String, String> documentIdToMessageMap;
+    private final HashMap<String, Exception> failedEntries;
 
-  public BatchIndexService(
-      S3Driver s3Driver, IndexingClient indexingClient, QueueClient queueClient) {
-    this.s3Driver = s3Driver;
-    this.indexingClient = indexingClient;
-    this.queueClient = queueClient;
-  }
+    public BatchIndexService(S3Driver s3Driver, IndexingClient indexingClient, QueueClient queueClient) {
+        this.s3Driver = s3Driver;
+        this.indexingClient = indexingClient;
+        this.queueClient = queueClient;
+        this.failedEntries = new HashMap<>();
+        this.documentIdToMessageMap = new HashMap<>();
+    }
 
-  @JacocoGenerated
-  public static BatchIndexService defaultService() {
-    return new BatchIndexService(
-        new S3Driver(S3Driver.defaultS3Client().build(), PERSISTED_RESOURCES_BUCKET),
-        IndexingClient.defaultIndexingClient(),
-        IndexQueueClient.defaultQueueClient());
-  }
+    @JacocoGenerated
+    public static BatchIndexService defaultService() {
+        return new BatchIndexService(new S3Driver(S3Driver.defaultS3Client().build(), PERSISTED_RESOURCES_BUCKET),
+                                     IndexingClient.defaultIndexingClient(), IndexQueueClient.defaultQueueClient());
+    }
 
-  public void batchIndex(Collection<String> messageBodies) {
-    var messages = messageBodies.stream().filter(Objects::nonNull).toList();
-    var failedEntriesMap = new HashMap<String, Exception>();
-    var documentsToIndex = new ArrayList<IndexDocument>();
+    public void batchIndex(Collection<String> messageBodies) {
+        var messages = messageBodies.stream().filter(Objects::nonNull).toList();
+        var documents = fetchDocuments(messages);
+        var response = attempt(() -> indexingClient.batchInsert(documents.stream()));
 
-    var documentIdToMessageMap = new HashMap<String, String>();
-
-    for (String message : messages) {
-      var eventReference = getEventReference(message);
-      if (eventReference.isFailure()) {
-        LOGGER.error(FAILED_TO_PARSE_SQS_MESSAGE, message, eventReference.getException());
-        failedEntriesMap.put(message, eventReference.getException());
-      } else {
-        var document = getDocumentFromS3(eventReference);
-        if (document.isFailure()) {
-          LOGGER.error(FAILED_TO_FETCH_FROM_S3_MESSAGE, message, document.getException());
-          failedEntriesMap.put(message, document.getException());
+        if (response.isFailure()) {
+            addAllDocumentsToFailedEntries(documents, response);
         } else {
-          documentsToIndex.add(document.get());
-          documentIdToMessageMap.put(document.get().getDocumentIdentifier(), message);
+            for (BulkItemResponse failedEntry : getFailedEntries(response.get())) {
+                addToFailedEntry(failedEntry, documents);
+            }
         }
-      }
+
+        failedEntries.forEach(this::sendToDlq);
     }
 
-    var response = attempt(() -> indexingClient.batchInsert(documentsToIndex.stream()));
+    private static Try<EventReference> getEventReference(String messageBody) {
+        return attempt(() -> JsonUtils.dtoObjectMapper.readValue(messageBody, EventReference.class));
+    }
 
-    if (response.isFailure()) {
-      for (IndexDocument indexDocument : documentsToIndex) {
-        failedEntriesMap.put(
-            documentIdToMessageMap.get(indexDocument.getDocumentIdentifier()),
-            response.getException());
-      }
-    } else {
-      var failedDocumentsUnderIndexing = getFailedEntries(response.get());
-      for (BulkItemResponse failedEntry : failedDocumentsUnderIndexing) {
+    private static MessageAttributeValue toAttributeValueString(Exception exception) {
+        return MessageAttributeValue.builder().stringValue(exception.getMessage()).dataType("String").build();
+    }
+
+    private static List<BulkItemResponse> getFailedEntries(Stream<BulkResponse> response) {
+        return response.filter(BulkResponse::hasFailures)
+                   .map(BulkResponse::getItems)
+                   .map(Arrays::asList)
+                   .flatMap(List::stream)
+                   .filter(BulkItemResponse::isFailed)
+                   .toList();
+    }
+
+    private static IndexDocument getDocumentByIdentifier(List<IndexDocument> indexDocuments, String documentId) {
+        return indexDocuments.stream()
+                   .filter(item -> item.getDocumentIdentifier().equals(documentId))
+                   .findFirst()
+                   .orElseThrow();
+    }
+
+    private static SendMessageRequest createDlqMessage(String messageBody, Exception exception) {
+        return SendMessageRequest.builder()
+                   .messageBody(messageBody)
+                   .messageAttributes(Map.of("exception", toAttributeValueString(exception)))
+                   .queueUrl(new Environment().readEnv(DLQ_QUEUE))
+                   .build();
+    }
+
+    private void addToFailedEntry(BulkItemResponse failedEntry, ArrayList<IndexDocument> documents) {
         var documentIdentifier = failedEntry.getId();
-        var document = getDocumentByIdentifier(documentsToIndex, documentIdentifier);
+        var document = getDocumentByIdentifier(documents, documentIdentifier);
         var message = documentIdToMessageMap.get(document.getDocumentIdentifier());
-        failedEntriesMap.put(message, failedEntry.getFailure().getCause());
-        LOGGER.error(
-            FAILED_DOCUMENT_MESSAGE, document.getDocumentIdentifier(), document.getIndexName());
-      }
+        failedEntries.put(message, failedEntry.getFailure().getCause());
+        LOGGER.error(FAILED_DOCUMENT_MESSAGE, document.getDocumentIdentifier(), document.getIndexName());
     }
 
-    failedEntriesMap.forEach(this::sendToDlq);
-  }
+    private void addAllDocumentsToFailedEntries(ArrayList<IndexDocument> documents,
+                                                Try<Stream<BulkResponse>> response) {
+        for (IndexDocument indexDocument : documents) {
+            failedEntries.put(documentIdToMessageMap.get(indexDocument.getDocumentIdentifier()),
+                              response.getException());
+        }
+    }
 
-  private static Try<EventReference> getEventReference(String messageBody) {
-    return attempt(() -> JsonUtils.dtoObjectMapper.readValue(messageBody, EventReference.class));
-  }
+    private ArrayList<IndexDocument> fetchDocuments(List<String> messages) {
+        var documentsToIndex = new ArrayList<IndexDocument>();
+        for (String message : messages) {
+            var eventReference = getEventReference(message);
+            if (eventReference.isFailure()) {
+                LOGGER.error(FAILED_TO_PARSE_SQS_MESSAGE, message, eventReference.getException());
+                failedEntries.put(message, eventReference.getException());
+            } else {
+                var document = getDocumentFromS3(eventReference);
+                if (document.isFailure()) {
+                    LOGGER.error(FAILED_TO_FETCH_FROM_S3_MESSAGE, message, document.getException());
+                    failedEntries.put(message, document.getException());
+                } else {
+                    documentsToIndex.add(document.get());
+                    documentIdToMessageMap.put(document.get().getDocumentIdentifier(), message);
+                }
+            }
+        }
+        return documentsToIndex;
+    }
 
-  private static MessageAttributeValue toAttributeValueString(Exception exception) {
-    return MessageAttributeValue.builder()
-        .stringValue(exception.getMessage())
-        .dataType("String")
-        .build();
-  }
+    private Try<IndexDocument> getDocumentFromS3(Try<EventReference> eventReference) {
+        return attempt(() -> fetchIndexDocumentFromS3(eventReference.get()));
+    }
 
-  private static List<BulkItemResponse> getFailedEntries(Stream<BulkResponse> response) {
-    return response
-        .filter(BulkResponse::hasFailures)
-        .map(BulkResponse::getItems)
-        .map(Arrays::asList)
-        .flatMap(List::stream)
-        .filter(BulkItemResponse::isFailed)
-        .toList();
-  }
+    private void sendToDlq(String messageBody, Exception exception) {
+        queueClient.sendMessage(createDlqMessage(messageBody, exception));
+    }
 
-  private static IndexDocument getDocumentByIdentifier(
-      List<IndexDocument> indexDocuments, String documentId) {
-    return indexDocuments.stream()
-        .filter(item -> item.getDocumentIdentifier().equals(documentId))
-        .findFirst()
-        .orElseThrow();
-  }
-
-  private static SendMessageRequest createDlqMessage(String messageBody, Exception exception) {
-    return SendMessageRequest.builder()
-        .messageBody(messageBody)
-        .messageAttributes(Map.of("exception", toAttributeValueString(exception)))
-        .queueUrl(new Environment().readEnv(DLQ_QUEUE))
-        .build();
-  }
-
-  private Try<IndexDocument> getDocumentFromS3(Try<EventReference> eventReference) {
-    return attempt(() -> fetchIndexDocumentFromS3(eventReference.get()));
-  }
-
-  private void sendToDlq(String messageBody, Exception exception) {
-    queueClient.sendMessage(createDlqMessage(messageBody, exception));
-  }
-
-  private IndexDocument fetchIndexDocumentFromS3(EventReference eventReference) {
-    var path = UriWrapper.fromUri(eventReference.getUri()).toS3bucketPath();
-    return IndexDocument.fromJsonString(s3Driver.getFile(path));
-  }
+    private IndexDocument fetchIndexDocumentFromS3(EventReference eventReference) {
+        var path = UriWrapper.fromUri(eventReference.getUri()).toS3bucketPath();
+        return IndexDocument.fromJsonString(s3Driver.getFile(path));
+    }
 }
