5b814a84f11d8fdc2e04de0a8efb32886ef8f218
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-01-07 10:33:02 +0100

    refactor: Add better logging for index create/delete operations

--- indexing-handlers/src/main/java/no/unit/nva/indexing/handlers/InitHandler.java
+++ indexing-handlers/src/main/java/no/unit/nva/indexing/handlers/InitHandler.java
@@ -6,16 +6,13 @@ import static no.unit.nva.constants.IndexMappingsAndSettings.TICKET_MAPPINGS;
 import static no.unit.nva.constants.Words.RESOURCES;
 import static no.unit.nva.constants.Words.TICKETS;
 import static no.unit.nva.indexingclient.IndexingClient.defaultIndexingClient;
-import static nva.commons.core.attempt.Try.attempt;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
 import no.unit.nva.indexing.model.IndexRequest;
 import no.unit.nva.indexingclient.IndexingClient;
 import nva.commons.core.JacocoGenerated;
-import nva.commons.core.attempt.Failure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,7 +21,7 @@ public class InitHandler implements RequestHandler<Object, String> {
   public static final String SUCCESS = "SUCCESS";
   public static final String FAILED = "FAILED. See logs";
 
-  private static final List<IndexRequest> INDEXES =
+  private static final List<IndexRequest> INDICES_TO_CREATE =
       List.of(
           new IndexRequest(RESOURCES, RESOURCE_MAPPINGS.asJson(), RESOURCE_SETTINGS.asJson()),
           new IndexRequest(TICKETS, TICKET_MAPPINGS.asJson()));
@@ -42,23 +39,22 @@ public class InitHandler implements RequestHandler<Object, String> {
 
   @Override
   public String handleRequest(Object input, Context context) {
+    var indexNames = INDICES_TO_CREATE.stream().map(IndexRequest::getName).toList();
+    logger.info("Starting index creation for indices: {}", indexNames);
 
-    var failState = new AtomicBoolean(false);
-
-    INDEXES.forEach(
-        request ->
-            attempt(
-                    () ->
-                        indexingClient.createIndex(
-                            request.getName(), request.getMappings(), request.getSettings()))
-                .orElse(fail -> handleFailure(failState, fail)));
-
-    return failState.get() ? FAILED : SUCCESS;
-  }
-
-  private Void handleFailure(AtomicBoolean failState, Failure<?> failure) {
-    failState.set(true);
-    logger.warn("Index creation failed", failure.getException());
-    return null;
+    boolean hasFailed = false;
+    for (var request : INDICES_TO_CREATE) {
+      var indexName = request.getName();
+      logger.info("Attempting to create index '{}'", indexName);
+      try {
+        indexingClient.createIndex(indexName, request.getMappings(), request.getSettings());
+        logger.info("Created index '{}'", indexName);
+      } catch (Exception exception) {
+        logger.error("Failed to create index '{}'", indexName, exception);
+        hasFailed = true;
+      }
+    }
+    logger.info("Index creation completed");
+    return hasFailed ? FAILED : SUCCESS;
   }
 }
