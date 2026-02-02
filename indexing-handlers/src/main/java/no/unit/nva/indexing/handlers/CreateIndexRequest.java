56fc3d3e9c7d4eed9234d94a29f42c62a7b5aa88
Kir Truhacev <kir.truhacev@sikt.no>
2026-02-02 10:35:30 +0100

    merging init handlers

---
+++ indexing-handlers/src/main/java/no/unit/nva/indexing/handlers/CreateIndexRequest.java
@@ -0,0 +1,45 @@
+package no.unit.nva.indexing.handlers;
+
+import static java.util.Objects.nonNull;
+import static no.unit.nva.commons.json.JsonUtils.dtoObjectMapper;
+import static nva.commons.core.attempt.Try.attempt;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Collections;
+import no.unit.nva.commons.json.JsonSerializable;
+import nva.commons.core.ioutils.IoUtils;
+
+public record CreateIndexRequest(Collection<IndexName> indices) implements JsonSerializable {
+
+  @SuppressWarnings("PMD.UnusedAssignment")
+  public CreateIndexRequest {
+    indices = nonNull(indices) ? indices : Collections.emptyList();
+  }
+
+  public static CreateIndexRequest fromInputStream(InputStream inputStream) {
+    return attempt(() -> IoUtils.streamToString(inputStream))
+        .map(value -> dtoObjectMapper.readValue(value, CreateIndexRequest.class))
+        .orElseThrow(
+            failure ->
+                new IllegalArgumentException("Could not parse request!", failure.getException()));
+  }
+
+  public enum IndexName {
+    RESOURCES("resources"),
+    TICKETS("tickets"),
+    IMPORT_CANDIDATES("import-candidate");
+
+    private final String value;
+
+    IndexName(String value) {
+      this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+      return value;
+    }
+  }
+}
--- indexing-handlers/src/main/java/no/unit/nva/indexing/handlers/IndexRequest.java
+++ indexing-handlers/src/main/java/no/unit/nva/indexing/handlers/IndexRequest.java
@@ -1,4 +1,4 @@
-package no.unit.nva.indexing.model;
+package no.unit.nva.indexing.handlers;
 
 import static nva.commons.core.attempt.Try.attempt;
 
@@ -7,38 +7,20 @@ import java.util.Collections;
 import java.util.Map;
 import no.unit.nva.commons.json.JsonUtils;
 
-public class IndexRequest {
-  private final String name;
-  private final Map<String, Object> mappings;
-  private final Map<String, Object> settings;
+public record IndexRequest(
+    String name, Map<String, Object> mappings, Map<String, Object> settings) {
 
   public IndexRequest(String name, String jsonMappings) {
-    this.name = name;
-    this.mappings = jsonToJavaMap(jsonMappings);
-    this.settings = Collections.emptyMap();
+    this(name, jsonToJavaMap(jsonMappings), Collections.emptyMap());
   }
 
   public IndexRequest(String name, String jsonMappings, String jsonSettings) {
-    this.name = name;
-    this.mappings = jsonToJavaMap(jsonMappings);
-    this.settings = jsonToJavaMap(jsonSettings);
+    this(name, jsonToJavaMap(jsonMappings), jsonToJavaMap(jsonSettings));
   }
 
   private static Map<String, Object> jsonToJavaMap(String jsonMappings) {
     var typeReference = new TypeReference<Map<String, Object>>() {};
     return attempt(() -> JsonUtils.dtoObjectMapper.readValue(jsonMappings, typeReference))
         .orElseThrow();
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public Map<String, Object> getMappings() {
-    return mappings;
-  }
-
-  public Map<String, Object> getSettings() {
-    return settings;
   }
 }
