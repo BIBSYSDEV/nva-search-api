b12b9b9d16b73deef21e6118f80c89088ad8d121
Kir Truhacev <107109909+truhacevkir@users.noreply.github.com>
2026-03-12 10:07:23 +0100

    extending isbn query param (#758)
    Extending isbn search parameter to filter documents which have parent
    with provided isbn.

--- search-commons/src/main/java/no/unit/nva/search/resource/Constants.java
+++ search-commons/src/main/java/no/unit/nva/search/resource/Constants.java
@@ -205,7 +205,9 @@ public final class Constants {
   public static final String PUBLISHER_ID_KEYWORD = jsonPath(PUBLISHER, ID, KEYWORD);
   public static final String STATUS_KEYWORD = jsonPath(STATUS, KEYWORD);
   public static final String PUBLICATION_CONTEXT_ISBN_LIST =
-      jsonPath(ENTITY_PUBLICATION_CONTEXT, ISBN_LIST);
+      multipleFields(
+          jsonPath(ENTITY_PUBLICATION_CONTEXT, ISBN_LIST),
+          jsonPath(ENTITY_PUBLICATION_CONTEXT, ENTITY_PUBLICATION_CONTEXT, ISBN_LIST));
   public static final String PUBLICATION_CONTEXT_TYPE_KEYWORD =
       jsonPath(ENTITY_PUBLICATION_CONTEXT, TYPE, KEYWORD);
   public static final String PUBLICATION_INSTANCE_TYPE =
