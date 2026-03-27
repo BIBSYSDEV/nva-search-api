6e98e5f400288b0dd15646a8f7c84f31d5bf218f
Lars-Olav Vågene <42863501+LarsV123@users.noreply.github.com>
2026-03-27 13:09:48 +0100

    wip: Enable logging

--- search-commons/src/main/java/no/unit/nva/search/common/OpenSearchClient.java
+++ search-commons/src/main/java/no/unit/nva/search/common/OpenSearchClient.java
@@ -129,7 +129,7 @@ public abstract class OpenSearchClient<R, Q extends Query<?>> {
   }
 
   protected HttpRequest createRequest(QueryContentWrapper qbs) {
-    logger.debug(qbs.body());
+    logger.info(qbs.body()); // FIXME: Temp test
     return HttpRequest.newBuilder(qbs.uri())
         .headers(
             ACCEPT, MediaType.JSON_UTF_8.toString(),
